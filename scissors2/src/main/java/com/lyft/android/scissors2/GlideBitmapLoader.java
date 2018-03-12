4cbcbe1fabae1d87c2d9fa7123d08dcec048e714
Anton Homeniuk <anton.homeniuk@gmail.com>
2018-03-12 12:16:16 +0300

    Update GlideBitmapLoader.java

--- scissors2/src/main/java/com/lyft/android/scissors2/GlideBitmapLoader.java
+++ scissors2/src/main/java/com/lyft/android/scissors2/GlideBitmapLoader.java
@@ -23,7 +23,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.RequestOptions;
 
 /**
